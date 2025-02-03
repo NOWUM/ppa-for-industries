179e82ba769d96da1391821f6e6ec391f9cbbe64
Jonathan <sejdija@fh-aachen.de>
2025-02-03 15:08:04 +0100

    added multi-processing

--- ppa_simulator/__main__.py
+++ ppa_simulator/__main__.py
@@ -3,32 +3,57 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 import os
+from multiprocessing import Pool
 
 from dotenv import load_dotenv
+from tqdm import tqdm
 
 from .simulator import Simulator
 
 
-def main(DB_URI: str, profile_id: int = None, scenario: list = [0.9, 0.95, 0.98, 1, 1.02, 1.05, 1.1]):
+def simulate_profile(args):
     """
-    Runs the simulation for the specified database URI, profile ID and scenario.
+    Wrapper-Funktion zur Simulation eines Profils mit einer bestimmten ID und einem Szenario.
+    """
+    db_uri, profile_id, scenario, start_date, end_date = args
+    simulator = Simulator(db_uri, start_date, end_date)
+    simulator.simulate(profile_id, scenario)
 
-    Parameters:
+
+def main(
+    DB_URI: str,
+    profile_id: int = None,
+    scenario: list = [0.9, 0.95, 0.98, 1, 1.02, 1.05, 1.1],
+):
+    """
+    Führt die Simulation für die angegebene Datenbank-URI, Profil-ID und das Szenario aus.
+
+    Parameter:
     ----------
     DB_URI : str
-        The URI of the database containing load profile data.
+        Die URI der Datenbank mit den Lastprofildaten.
     profile_id : int
-        The ID of the load profile to simulate.
+        Die ID des zu simulierenden Lastprofils.
     scenario : list
-        A list containing factors that the market price is multiplied by, simulating market price volatility.
+        Eine Liste mit Faktoren zur Simulation von Marktpreisschwankungen.
     """
     start_date, end_date = "2019-01-01", "2020-01-01"
-    simulator = Simulator(DB_URI, start_date, end_date)
+
     if profile_id is None:
-        for i in range(5359):
-            simulator.simulate(i, scenario)
+        # Anzahl der Profile und Argumente für Multiprocessing vorbereiten
+        num_profiles = 5359  # Gesamtanzahl der Profile
+        args_list = [
+            (DB_URI, i, scenario, start_date, end_date) for i in range(num_profiles)
+        ]
+
+        # Fortschrittsbalken initialisieren
+        with Pool() as pool:
+            with tqdm(total=num_profiles) as pbar:
+                for _ in pool.imap_unordered(simulate_profile, args_list):
+                    pbar.update(1)
     else:
-        simulator.simulate(profile_id)
+        # Simulation für ein einzelnes Profil ausführen
+        simulate_profile((DB_URI, profile_id, scenario, start_date, end_date))
 
 
 if __name__ == "__main__":
